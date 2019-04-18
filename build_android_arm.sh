192ca166084c272a1339ace2b82798c3eaf2b35f
Ubuntu <ubuntu@ip-172-31-29-229.ap-south-1.compute.internal>
2019-04-18 11:57:38 +0000

    script file for android make

---
+++ build_android_arm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+NDK=/home/ubuntu/android-ndk-r13b
+PLATFORM=$NDK/platforms/android-16/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+PREFIX=./android/arm
+
+function build_one
+{
+  ./configure \
+  --prefix=$PREFIX \
+  --enable-static \
+  --enable-pic \
+  --host=arm-linux \
+  --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+  --sysroot=$PLATFORM
+
+  make clean
+  make
+  make install
+}
+
+build_one
+
+echo Android ARM builds finished
